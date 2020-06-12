128f4c6845fd815fa574c691170cfc488f452341
Tomas Prado <tomas.p@admincentralmail.com>
2020-06-12 17:21:16 +0200

    Solution

--- src/processor.js
+++ src/processor.js
@@ -1,15 +1,59 @@
 (function (exports) {
     /**
      * Returns the new map state
-     * @param {number[]} array 
+     * @param {number[][]} array 
      */
     function _process(array) {
-        // TODO
         // En una celda nace un ser vivo si  tiene 3 celdas vecinas con seres vivos.    
         // Una celda con un ser vivo, sigue viviendo si tiene dos o tres vecinos vivos. Si no, muere por soledad (<2)  o por sobrepoblación (>3)    
 
-        return array;
+        const returnArray = [];
+        for (let i = 0; i < array.length; i++) {
+            if (!returnArray[i]) {
+                returnArray[i] = [];
+            }
+            for (let j = 0; j < array[i].length; j++) {
+                const element = array[i][j];
+                returnArray[i][j] = _getNewState(array[i][j], _countNeighbors(i, j, array));
+            }
+        }
+
+        return returnArray;
     }
+
+    function _countNeighbors(i, j, array) {
+        return _coalesque(i - 1, j - 1, array) +
+            _coalesque(i - 1, j, array) +
+            _coalesque(i - 1, j + 1, array) +
+            _coalesque(i, j - 1, array) +
+            _coalesque(i, j + 1, array) +
+            _coalesque(i + 1, j - 1, array) +
+            _coalesque(i + 1, j, array) +
+            _coalesque(i + 1, j + 1, array);
+    }
+
+    function _coalesque(i, j, array) {
+        if (array[i] === undefined || array[i][j] === undefined) {
+            return 0;
+        }
+
+        return array[i][j] ? 1 : 0;
+    }
+
+    function _getNewState(state, neighbors) {
+        if (neighbors > 3 || neighbors < 2) {
+            return 0;
+        }
+        else if (neighbors === 3) {
+            return 1;
+        }
+
+        return state;
+    }
+
     exports.process = _process;
+    exports.countNeighbors = _countNeighbors;
+    exports.coalesque = _coalesque;
+    exports.getNewState = _getNewState;
 
 })(typeof module === 'object' ? module.exports : window);
--- test/processorTest.js
+++ test/processorTest.js
@@ -7,4 +7,39 @@ suite('ProcessorTest', () => {
         const actual = process.process([[1, 1]]);
         assert.deepEqual([[0, 0]], actual);
     });
+
+    test('when_a_cell_has_3_neighbors_should_be_alive', () => {
+        const actual = process.process([
+            [1, 1],
+            [1, 1]
+        ]);
+        assert.deepEqual([
+            [1, 1],
+            [1, 1]
+        ], actual);
+    });
+
+    test('when_coalesque_called_with_undefined_should_return_0', () => {
+        assert.equal(0, process.coalesque(1, 1, [[]]));
+    });
+
+    test('when_coalesque_called_with_0_should_return_0', () => {
+        assert.equal(0, process.coalesque(0, 0, [[0]]));
+    });
+
+    test('when_coalesque_called_with_1_should_return_1', () => {
+        assert.equal(1, process.coalesque(0, 1, [[0, 1]]));
+    });
+
+    test('when_countNeighbors_called_with_two_neighbors_should_return_2', () => {
+        assert.equal(2, process.countNeighbors(0, 1, [[1, 1, 1]]));
+    });
+
+    test('when_countNeighbors_called_with_no_neighbors_should_return_0', () => {
+        assert.equal(0, process.countNeighbors(0, 0, [[]]));
+    });
+
+    test('when_countNeighbors_called_with_one_neighbors_should_return_1', () => {
+        assert.equal(1, process.countNeighbors(0, 0, [[1, 1]]));
+    });
 });
