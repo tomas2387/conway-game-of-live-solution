d95a2e9b50b923a42be3fb97fe250102f61175e3
Tomas Prado <tomas.p@admincentralmail.com>
2020-06-12 16:21:41 +0200

    Array bidimiensional

--- test/processorTest.js
+++ test/processorTest.js
@@ -4,7 +4,7 @@ const { assert } = require('chai');
 suite('ProcessorTest', () => {
 
     test('when_a_cell_has_one_neightbor_should_die', () => {
-        const actual = process.process([1, 1]);
-        assert.equal([0, 0], actual);
+        const actual = process.process([[1, 1]]);
+        assert.equal([[0, 0]], actual);
     });
 });
