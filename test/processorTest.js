dd28b5143fe78b3837aa33b42b1c5317e19a28f3
Tomas Prado <tomas.p@admincentralmail.com>
2020-06-12 16:26:58 +0200

    cambiar assert.equal assert.deepEqual

--- test/processorTest.js
+++ test/processorTest.js
@@ -5,6 +5,6 @@ suite('ProcessorTest', () => {
 
     test('when_a_cell_has_one_neightbor_should_die', () => {
         const actual = process.process([[1, 1]]);
-        assert.equal([[0, 0]], actual);
+        assert.deepEqual([[0, 0]], actual);
     });
 });
